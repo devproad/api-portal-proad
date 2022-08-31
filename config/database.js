4abd96b1899be025fea94d00b0a7389c9a16f11f
Rúben Filipe <rubenfilipe.net@gmail.com>
2022-08-31 10:18:39 -0300

    changing database to MySQL

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'web-worrel.info.ufrn.br'),
+      port: env.int('DATABASE_PORT', 8085),
+      database: env('DATABASE_NAME', 'proad'),
+      user: env('DATABASE_USERNAME', 'webproad'),
+      password: env('DATABASE_PASSWORD', 'proad2022-b4EOpSB071'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
+    acquireConnectionTimeout: 1000000,
+    options: {
+      debug: false,
+      pool: {
+        min: 2,
+        max: 10,
+        createTimeoutMillis: 30000,
+        acquireTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 30000,
+        createRetryIntervalMillis: 30000,
+        propagateCreateError: false,
+      },
+    },
+    
   },
 });
